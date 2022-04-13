64a4a7167d9710e1bd186463bb73c9742a296c6d
EC2 Default User <ec2-user@ip-172-31-28-133.ec2.internal>
2022-04-13 09:34:37 +0000

    [Update]4-25

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ devise_for :users
  get '/homes/about' => 'homes#about', as: 'about'
 
 get 'books/index'
-resource :books, only:[:new, :index, :show, :edit]
+resource :books, only:[:new, :index, :show, :edit, :create]
 resource :users, only:[:show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
